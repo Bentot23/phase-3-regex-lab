15ec66336ef8a6103ba9ff80b9ce3198e81ac075
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-12-02 15:01:31 -0600

    commit tree f59f24a757eb41c14c77725d8940cf374af10ade

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,24 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    word.match(/^[AEIOU]|^[aeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un\S*/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/^\w{5}\s/).count
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match(/[^A_Z]+[!@#\\$%\\^\\&*\\)\\(+=._-]$/) ? true : false
+    # else text.scan(/[^a_z]+[!@#\\$%\\^\\&*\\)\\(+=._-]$/)
+    #     false
+    # end
 end
 
 def valid_phone_number?(phone)
-
+    phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 end
+# binding.pry
